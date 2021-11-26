fa456aeecb0ff0b8368efe8ff82ecd3cb504a13d
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-11-26 03:00:47 +0300

    Update eERC.sol

--- eERC.sol
+++ eERC.sol
@@ -1,17 +1,16 @@
-/**
- *Submitted for verification at FtmScan.com on 2021-11-22
-*/
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
 // A modification of OpenZeppelin ERC20
 // Original can be found here: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol
+//interface I{
+//	function sync() external;
+//}
 
 contract eERC {
 	event Transfer(address indexed from, address indexed to, uint value);
 	event Approval(address indexed owner, address indexed spender, uint value);
-	event BulkTransfer(address indexed from, address[] indexed recipients, uint[] amounts);
+	//event BulkTransfer(address indexed from, address[] indexed recipients, uint[] amounts);
 
 	mapping (address => mapping (address => bool)) private _allowances;
 	mapping (address => uint) private _balances;
@@ -23,17 +22,21 @@ contract eERC {
     uint public epochBlock;
     address public pool;
     bool public ini;
-    
+    uint public burnBlock;
+    uint public burnModifier;
+
 	function init() public {
-	    require(ini==true);ini=false;
+	    require(ini==false);ini=true;
 		//_treasury = 0xeece0f26876a9b5104fEAEe1CE107837f96378F2;
 		//_founding = 0xAE6ba0D4c93E529e273c8eD48484EA39129AaEdc;
 		//_staking = 0x0FaCF0D846892a10b1aea9Ee000d7700992B64f8;
 		//emit Transfer(0x6B51c705d1E78DF8f92317130a0FC1DbbF780a5A,0xeece0f26876a9b5104fEAEe1CE107837f96378F2,29e23);
 	    //_balances[pool] = _balances[pool] - 46762716725205235873429;
-	    _balances[pool] = 92365947461693200000000;
+	    //_balances[pool] = 92365947461693200000000;
 	    //_balances[0xeece0f26876a9b5104fEAEe1CE107837f96378F2] += 46762716725205235873429;
 	    //emit Transfer(pool,0xeece0f26876a9b5104fEAEe1CE107837f96378F2,46762716725205235873429);
+		burnBlock = epochBlock;
+		burnModifier = 500;
 	}
 	
 //	function genesis(uint b, address p) public {
@@ -106,8 +109,11 @@ contract eERC {
 	}
 
 	function _burn(uint amount) internal {
+		require(_balances[pool] > amount);
 		_balances[pool] -= amount;
 		_balances[0xeece0f26876a9b5104fEAEe1CE107837f96378F2]+=amount;//treasury
+		emit Transfer(pool,0xeece0f26876a9b5104fEAEe1CE107837f96378F2,amount);
+		//I(pool).sync(); // in general it's better not to have it with small amounts. exchanging a bit of accuracy for less ftm drainage. however staking contract might need to sync
 	}
 
 	function _transfer(address sender, address recipient, uint amount) internal {
@@ -139,7 +145,7 @@ contract eERC {
 		uint senderBalance = _balances[msg.sender];
 		uint total;
 		for(uint i = 0;i<amounts.length;i++) {
-		    total += amounts[i];
+		    total += amounts[i];// careful, it does not burn anything as regular transfer does
 		    _balances[recipients[i]] += amounts[i];
 		}
 		require(senderBalance >= total,"balance is low");
@@ -170,5 +176,16 @@ contract eERC {
 //				withdrawn+=amount;
 //			}
 //		}
+		uint pB = _balances[pool];
+		if(pB > 1e22 && block.number >= burnBlock) {
+			uint toBurn = pB*10/burnModifier;
+			burnBlock+=86400;
+			_burn(toBurn);
+		}
+	}
+
+	function setBurnModifier(uint amount) external {
+		require(msg.sender == 0x5C8403A2617aca5C86946E32E14148776E37f72A && amount>=200 && amount<10000);
+		burnModifier = amount;
 	}
 }
